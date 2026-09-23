690d4905150136ef9368b07aa1573b1f7ce769dc
Saumy003 <saumysingh2005@gmail.com>
2026-09-23 19:01:02 +0530

    Concatenating function

--- Merging, Joining & Concatenating/concat.py
+++ Merging, Joining & Concatenating/concat.py
@@ -10,3 +10,19 @@ dec = pd.read_csv(r"D:\PANDAS\Merging, Joining & Concatenating\reg-month2.csv")
 matches = pd.read_csv(r"D:\PANDAS\Merging, Joining & Concatenating\matches.csv")
 deliveries = pd.read_csv(r"D:\PANDAS\Merging, Joining & Concatenating\deliveries.csv")
 
+# concat -> vertically mearge two or more dataframes
+print(pd.concat([nov, dec]))
+
+# ignore_index
+print(pd.concat([nov, dec],ignore_index=True))
+
+# concat as multi-index dataframe
+print(pd.concat([nov, dec],keys=['Nov', 'Dec']))
+
+# Multi-index Dataframe -> fetch using iloc
+multi = pd.concat([nov, dec], keys=["Nov", "Dec"])
+print(multi.loc[("Nov", 0)])
+print(multi.loc[("Dec", 4)])
+
+# concat dataframes horizontally
+print(pd.concat([nov, dec], axis= 1))
