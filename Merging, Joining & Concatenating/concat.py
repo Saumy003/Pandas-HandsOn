34ce6ea53f9ed0cd48a90f135c1cd22aba2288a2
Saumy003 <saumysingh2005@gmail.com>
2026-09-23 17:20:58 +0530

    Sample Data Set we used in Merging, joining & Concatenating

---
+++ Merging, Joining & Concatenating/concat.py
@@ -0,0 +1,12 @@
+""" Topic 1. Concat function """
+
+import numpy as np
+import pandas as pd
+
+courses = pd.read_csv(r"D:\PANDAS\Merging, Joining & Concatenating\courses.csv")
+students = pd.read_csv(r"D:\PANDAS\Merging, Joining & Concatenating\students.csv")
+nov = pd.read_csv(r"D:\PANDAS\Merging, Joining & Concatenating\reg-month1.csv")
+dec = pd.read_csv(r"D:\PANDAS\Merging, Joining & Concatenating\reg-month2.csv")
+matches = pd.read_csv(r"D:\PANDAS\Merging, Joining & Concatenating\matches.csv")
+deliveries = pd.read_csv(r"D:\PANDAS\Merging, Joining & Concatenating\deliveries.csv")
+
